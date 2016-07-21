b1b7d70904a3b2699c5c0a03438e740ee14aa6eb
wang lei <398758695@qq.com>
2016-07-21 15:52:00 +0800

    could cp dir

--- makefile
+++ makefile
@@ -1,4 +1,8 @@
-all:
+all:find mycp
+.PHONY:all	
+find:find.c
 	gcc myfind.c -o find
+mycp:mycp.c
+	gcc mycp.c -o mycp
 clean:
 	rm find
--- mycp.c
+++ mycp.c
@@ -4,6 +4,7 @@
 #include<string.h>
 #include<fcntl.h>
 #include<sys/stat.h>
+#include<unistd.h>
 #define MAXSIZE 1024
 void copydata(char *src,char *des)
 {
@@ -26,11 +27,51 @@ void copydata(char *src,char *des)
     close(sfd);
     close(dfd);
 }
-
+void copydir(char *src,char *des,struct stat src_stat)
+{
+    DIR *dp;
+    struct dirent *dirp;
+    char *spathname,*dpathname;
+    dp=opendir(src);
+    if(mkdir(des,src_stat.st_mode)==-1)
+    {
+        printf("mkdir %s error,maybe the dir has being exsit!\n",des);
+    }
+    while(dirp=readdir(dp))
+    {
+        if (!strcmp(dirp->d_name, ".") || !strcmp(dirp->d_name, ".."))
+			{
+				continue;
+			}
+        spathname = (char *)malloc(2+strlen(src+strlen(dirp->d_name)));
+        dpathname = (char *)malloc(2+strlen(des+strlen(dirp->d_name)));
+        sprintf(spathname,"%s/%s",src,dirp->d_name);
+        sprintf(dpathname,"%s/%s",des,dirp->d_name);
+    
+        if(stat(spathname,&src_stat)==-1)
+        {
+          printf("stat error\n");
+        }
+        if(S_ISDIR(src_stat.st_mode))
+            copydir(spathname,dpathname,src_stat);
+        else
+            copydata(spathname,dpathname);
+        free(spathname);
+        free(dpathname);
+    }
+}
 int main(int argc,char **argv)
 {
-    if(argc<3)
+     if(argc<3)
         printf("usage: cp filename filename \n");
-    copydata(argv[1],argv[2]);
+   struct stat src_stat;
+   if(stat(argv[1],&src_stat)==-1)
+    {
+        printf("stat error\n");
+    }
+    if(S_ISDIR(src_stat.st_mode))
+            copydir(argv[1],argv[2],src_stat);
+        else
+            copydata(argv[1],argv[2]);
     return 0;
 }
