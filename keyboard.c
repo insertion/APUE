cf4f2771235faed0bb5bc8dc457ff572a8cf5a5a
wang lei <398758695@qq.com>
2016-07-26 18:41:02 +0800

    add arg

--- keyboard.c
+++ keyboard.c
@@ -21,13 +21,42 @@ void daemon()
 int main(int argc,char ** argv)
 {
     int fd;
+    char ch;
     struct input_event kevent;
+    char *usage = "usage\n\t-b : run in background\n\t-f : output to a file\n";
     if((fd=open("/dev/input/event2",O_RDONLY))==-1)
     {
         printf("usage: you must be root!\n");
         exit(0);
     }
-    daemon(0,0);
+    /*
+     *int getopt(int argc, char * const argv[], const char *optstring);
+     *a 后没有冒号，表示没有可以参数 
+     *b 后跟一个冒号，表示有一个必要的参数 
+     *c 后跟两个冒号，表示有一个可选的参数 
+     *
+     */ 
+    while( ( ch = getopt( argc, argv, "hbf:" ) ) != EOF ) 
+    {
+        //test for getopt
+        switch(ch) 
+        {
+            case 'h':
+                printf("%s",usage);
+                exit(0);
+                break;
+            case 'b':
+                daemon(0,0);
+                break;
+            case 'f':
+                printf("the arg is %s\n",optarg);
+                break;
+            default:
+                break;
+
+        }
+    }
+   
     /*
     *extern int daemon(int nochdir,int noclose);
     *run in background,
